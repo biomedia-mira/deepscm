51694fb06229e54b5ace8fe0d7897d5497598efe
Daniel Coelho de Castro <d.coelho-de-castro15@imperial.ac.uk>
2019-11-27 20:13:01 +0000

    Implement products of distributions with auto-dispatch

--- distributions/products.py
+++ distributions/products.py
@@ -1,58 +1,143 @@
+from typing import Sequence, Tuple
+
 import torch
-import torch.distributions as td
+from torch import Tensor
+from torch.distributions import Distribution, Categorical, MultivariateNormal
 
-from util import posdef_inverse
+from distributions.factorised import Factorised
+from distributions.natural_mvn import NaturalMultivariateNormal
+from util import posdef_solve
 
-
-def categorical_categorical(p: td.Categorical, q: td.Categorical):
-    return td.Categorical(logits=p.logits + q.logits)
-
-
-def mvn_mvn(p: td.MultivariateNormal, q: td.MultivariateNormal):
-    pass
+_PROD_REGISTRY = {}
 
 
-class ConjugateExponentialFamily:
-    def a(self):
-        raise NotImplementedError
+def register_product(type_p, type_q):
+    """
+    Decorator to register a pairwise function with :meth:`product`.
+    Usage::
+
+        @register_product(Normal, Normal)
+        def prod_normal_normal(p, q):
+            # insert implementation here
+
+    When `product(p, q)` is called, the lookup will attempt to match either `(type_p, type_q)` or
+    `(type_q, type_p)`.
+
+    Args:
+        type_p (type): A subclass of :class:`~torch.distributions.Distribution`.
+        type_q (type): A subclass of :class:`~torch.distributions.Distribution`.
+    """
+    if not isinstance(type_p, type) and issubclass(type_p, Distribution):
+        raise TypeError(f"Expected type_p to be a Distribution subclass but got {type_p}")
+    if not isinstance(type_q, type) and issubclass(type_q, Distribution):
+        raise TypeError(f"Expected type_q to be a Distribution subclass but got {type_q}")
+
+    def decorator(fun):
+        _PROD_REGISTRY[type_p, type_q] = fun
+        return fun
+
+    return decorator
 
 
-def condition_mvn(mvn: td.MultivariateNormal, y_dims, x_dims):
-    """Conditional distribution of Y|X"""
-    x_dims = torch.as_tensor(x_dims)#.unsqueeze(0)
-    y_dims = torch.as_tensor(y_dims)#.unsqueeze(0)
-    # TODO: Correctly handle single dimensions (int)
-    mx = mvn.loc[..., x_dims]  # (..., Dx)
-    my = mvn.loc[..., y_dims]  # (..., Dy)
-    Sxx = mvn.covariance_matrix[..., x_dims.unsqueeze(-1), x_dims.unsqueeze(-2)]  # (..., Dx, Dx)
-    Sxy = mvn.covariance_matrix[..., x_dims.unsqueeze(-1), y_dims.unsqueeze(-2)]  # (..., Dx, Dy)
-    Syy = mvn.covariance_matrix[..., y_dims.unsqueeze(-1), y_dims.unsqueeze(-2)]  # (..., Dy, Dy)
-    Syx = Sxy.transpose(-2, -1)  # (..., Dy, Dx)
-    if x_dims.dim() > 0:
-        Syx_iSxx = Syx @ posdef_inverse(Sxx)[0]  # (..., Dy, Dx)
-    else:
-        Syx_iSxx = Syx / Sxx  # (..., Dy, Dx)
-    Syy_x = Syy - Syx_iSxx @ Sxy  # (..., Dy, Dy)
-    def condition(x):
-        my_x = my + Syx_iSxx @ (x - mx)  # (..., Dy)
-        if y_dims.dim() > 0:
-            return td.MultivariateNormal(my_x, covariance_matrix=Syy_x)
-        else:
-            return td.Normal(my_x.squeeze(-1), torch.sqrt(Syy_x).squeeze(-1))
-    return condition
+def product(p: Distribution, q: Distribution, expand=True) -> Tuple[Distribution, Tensor]:
+    """Computes the product of two distributions.
+
+    Computes in closed form the distribution :math:`r(x)` and log-normalising constant
+    :math:`\log Z` such that :math:`r(x) = p(x) \cdot q(x) / Z`.
+
+    Args:
+        p (Distribution): A distribution.
+        q (Distribution): Another distribution.
+        expand (bool): Whether to perform the outer product of the batch shapes. If ``True``,
+            the output batch shape will be ``p.batch_shape + q.batch_shape``. Otherwise, it will be
+            equal to the input batch shapes, which must be the same.
+
+    Returns:
+        (Distribution, Tensor): The resulting distribution :math:`r(x)` and the log-normalising
+            constant :math:`\log Z`.
+
+    Raises:
+        NotImplementedError: If the distribution types have not been registered via
+            :meth:`register_product`.
+    """
+    if p.event_shape != q.event_shape:
+        raise ValueError("Cannot compute the product of distributions with different event shapes")
+    type_p, type_q = type(p), type(q)
+    try:
+        prod_fcn = _PROD_REGISTRY[type_p, type_q]
+        p_shape, q_shape = _broadcast_shapes(p.batch_shape, q.batch_shape, expand)
+        return prod_fcn(p, q, p_shape, q_shape)
+    except KeyError:
+        try:
+            prod_fcn = _PROD_REGISTRY[type_q, type_p]
+            q_shape, p_shape = _broadcast_shapes(q.batch_shape, p.batch_shape, expand)
+            return prod_fcn(q, p, q_shape, p_shape)
+        except KeyError:
+            raise NotImplementedError(f"No product implemented for {type_p} vs. {type_q}")
 
 
-class ClosedExponentialFamily(td.ExponentialFamily):
-    def product(self, other: 'ClosedExponentialFamily') -> 'ClosedExponentialFamily':
-        pass
+def _broadcast_shapes(p_shape, q_shape, expand):
+    if not expand:
+        if p_shape != q_shape:
+            raise ValueError(f"Equal batch shapes expected, got {p_shape} vs. {q_shape}")
+        return p_shape, q_shape
+    p_empty_shape = torch.Size([1] * len(p_shape))
+    q_empty_shape = torch.Size([1] * len(q_shape))
+    p_new_shape = p_shape + q_empty_shape
+    q_new_shape = p_empty_shape + q_shape
+    return p_new_shape, q_new_shape
 
 
-if __name__ == '__main__':
-    D = 5
-    mean = torch.zeros(D)
-    cov = torch.eye(D)
-    mvn = td.MultivariateNormal(mean, cov)
-    print(condition_mvn(mvn, (1,), (0,))(mean[0]))
-    print(condition_mvn(mvn, (1,), 0)(mean[0]))
-    print(condition_mvn(mvn, 1, (0,))(mean[0]))
-    print(condition_mvn(mvn, [1, 2], [0])(mean[[0]]))
+def _reshape_batch(tensors: Sequence[torch.Tensor], old_shape: torch.Size, new_shape: torch.Size):
+    if old_shape == new_shape:
+        return tensors
+    return [t.reshape(new_shape + t.shape[len(old_shape):]) for t in tensors]
+
+
+@register_product(Categorical, Categorical)
+def _prod_categorical_categorical(p: Categorical, q: Categorical, p_shape, q_shape):
+    p_logits, = _reshape_batch([p.logits], p.batch_shape, p_shape)
+    q_logits, = _reshape_batch([q.logits], q.batch_shape, q_shape)
+
+    pq_logits = p_logits + q_logits
+    pq = Categorical(logits=pq_logits)
+    pq_lognorm = pq_logits.logsumexp(-1) - p_logits.logsumexp(-1) - q_logits.logsumexp(-1)
+
+    return pq, pq_lognorm
+
+
+@register_product(MultivariateNormal, MultivariateNormal)
+def _prod_mvn_mvn(p: MultivariateNormal, q: MultivariateNormal, p_shape, q_shape):
+    p_mean, p_prec, p_cov = _reshape_batch([p.mean, p.precision_matrix, p.covariance_matrix],
+                                           p.batch_shape, p_shape)
+    q_mean, q_prec, q_cov = _reshape_batch([q.mean, q.precision_matrix, q.covariance_matrix],
+                                           q.batch_shape, q_shape)
+
+    pq_prec = p_prec + q_prec
+    pq_mean = posdef_solve(p_prec @ p_mean[..., None] + q_prec @ q_mean[..., None],
+                           pq_prec)[0].squeeze(-1)
+
+    pq = MultivariateNormal(pq_mean, precision_matrix=pq_prec)
+    pq_lognorm = MultivariateNormal(q_mean, p_cov + q_cov).log_prob(p_mean)
+
+    return pq, pq_lognorm
+
+
+@register_product(NaturalMultivariateNormal, NaturalMultivariateNormal)
+def _prod_nmvn_nmvn(p: NaturalMultivariateNormal, q: NaturalMultivariateNormal, p_shape, q_shape):
+    p_eta1, p_eta2 = _reshape_batch([p.nat_param1, p.nat_param2], p.batch_shape, p_shape)
+    q_eta1, q_eta2 = _reshape_batch([q.nat_param1, q.nat_param2], q.batch_shape, q_shape)
+
+    pq_eta1 = p_eta1 + q_eta1
+    pq_eta2 = p_eta2 + q_eta2
+
+    pq = NaturalMultivariateNormal(pq_eta1, pq_eta2)
+    pq_lognorm = pq.log_normalizer - p.log_normalizer.reshape(p_shape) \
+                 - q.log_normalizer.reshape(q_shape)
+
+    return pq, pq_lognorm
+
+
+@register_product(NaturalMultivariateNormal, MultivariateNormal)
+def _prod_nmvn_mvn(p: NaturalMultivariateNormal, q: MultivariateNormal, p_shape, q_shape):
+    return _prod_nmvn_nmvn(p, NaturalMultivariateNormal.from_standard(q), p_shape, q_shape)
