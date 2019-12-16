c3737a3f974f1acaa3611ba6425fa27634215cf8
Nick Pawlowski <pawlowski.nick@gmail.com>
2019-12-16 17:25:58 +0100

    rename enc/dec and include opt

--- models/sin.py
+++ models/sin.py
@@ -1,29 +1,32 @@
+import torch
 from torch import nn
 
 from .mixture import Mixture
 
 
 class MixtureSIN(nn.Module):
-    def __init__(self, prior_mixture: Mixture, decoder, encoder, var_mixture: Mixture):
-        self.prior_mixture = prior_mixture
-        self.decoder = decoder
-        self.encoder = encoder
-        self.var_mixture = var_mixture
+    def __init__(self, prior_mixture: Mixture, encoder, decoder, var_mixture: Mixture):
         super().__init__()
+        self.prior_mixture = prior_mixture
+        self.dec = decoder
+        self.enc = encoder
+        self.var_mixture = var_mixture
 
     def forward(self, data):
-        potentials = self.encoder.posterior(data)
+        potentials = self.enc.posterior(data)
         posteriors = self.var_mixture.posterior(potentials)
         latents = posteriors.rsample()
-        likelihoods = self.decoder.likelihood(latents)
+        likelihoods = self.dec.likelihood(latents)
         return potentials, posteriors, latents, likelihoods
 
 
-class SINTrainer(nn.Module):
-    def __init__(self, model: MixtureSIN, opt):
+class Trainer(object):
+    def __init__(self, model: MixtureSIN, lr: float = 1e-4):
         super().__init__()
         self.model = model
-        self.opt = opt
+
+        params = list(self.model.enc.parameters()) + list(self.model.dec.parameters()) + list(self.model.var_mixture.parameters())
+        self.opt = torch.optim.Adam(params, lr=lr, betas=(.5, .99))
 
     def step(self, data, verbose=False):
         potentials, posteriors, latents, likelihoods = self.model(data)
