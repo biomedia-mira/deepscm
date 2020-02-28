7143c759ba01d998ebd0f1ed7802d65ede254af6
Daniel Coelho de Castro <d.coelho-de-castro15@imperial.ac.uk>
2020-02-28 11:38:31 +0000

    Add docs and default columns to MorphoMNISTLike

--- datasets/morphomnist/__init__.py
+++ datasets/morphomnist/__init__.py
@@ -9,9 +9,16 @@ from morphomnist import io
 
 class MorphoMNISTLike(Dataset):
     def __init__(self, root_dir, train: bool = True, columns=None):
+        """
+        Args:
+            root_dir: path to data directory
+            train: whether to load the training subset (``True``, ``'train-*'`` files) or the test
+                subset (``False``, ``'t10k-*'`` files)
+            columns: list of morphometrics to load; by default (``None``) loads the image index and
+                all available metrics: area, length, thickness, slant, width, and height
+        """
         self.root_dir = root_dir
         self.train = train
-        self.columns = columns
         prefix = "train" if train else "t10k"
         images_filename = prefix + "-images-idx3-ubyte.gz"
         labels_filename = prefix + "-labels-idx1-ubyte.gz"
@@ -19,7 +26,10 @@ class MorphoMNISTLike(Dataset):
         self.images = torch.as_tensor(io.load_idx(os.path.join(self.root_dir, images_filename)))
         self.labels = torch.as_tensor(io.load_idx(os.path.join(self.root_dir, labels_filename)))
         metrics_df = pd.read_csv(os.path.join(root_dir, metrics_filename), usecols=columns)
+        if columns is None:
+            columns = metrics_df.columns
         self.metrics = {col: torch.as_tensor(metrics_df[col]) for col in columns}
+        self.columns = columns
         assert len(self.images) == len(self.labels) and len(self.images) == len(metrics_df)
 
     def __len__(self):
@@ -38,6 +48,7 @@ if __name__ == '__main__':
     # Example usage
     dataset = MorphoMNISTLike(root_dir="/vol/biomedic/users/dc315/mnist/original",
                               columns=['slant', 'thickness'], train=False)
+    print(dataset.columns)
     data_loader = DataLoader(dataset, batch_size=10, shuffle=True)
 
     for batch in data_loader:
