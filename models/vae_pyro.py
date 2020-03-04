76e3635d889afb5f13cfb2fc225ba22315bd547d
Daniel Coelho de Castro <d.coelho-de-castro15@imperial.ac.uk>
2020-03-04 15:29:04 +0000

    Update Pyro-compat. VAE to use arbitrary DeepConditionals

--- models/vae_pyro.py
+++ models/vae_pyro.py
@@ -1,44 +1,56 @@
 import pyro
-import pyro.distributions as dist
 import torch
-from pyro.infer import SVI, Trace_ELBO
-from pyro.optim import Adam
+from pyro.distributions import Normal
 from torch import nn
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 
-from models.vae import Decoder, Encoder
-from scripts import data_util
+from distributions.deep import DeepConditional
 
 
 class VAE(nn.Module):
-    def __init__(self, latent_dim):
+    def __init__(self, decoder: DeepConditional, encoder: DeepConditional,
+                 hidden_dim: int, latent_dim: int):
         super().__init__()
+        self.hidden_dim = hidden_dim
         self.latent_dim = latent_dim
-        self.encoder = Encoder(latent_dim)
-        self.decoder = Decoder(latent_dim)
+        self.decoder = decoder
+        self.encoder = encoder
 
     def model(self, x: torch.Tensor):
-        pyro.module('decoder', self.decoder)
+        decoder = pyro.module('decoder', self.decoder)
         with pyro.plate('observations', x.shape[0]):
             z_loc = x.new_zeros(self.latent_dim)
             z_scale = x.new_ones(self.latent_dim)
-            z = pyro.sample('z', dist.Normal(z_loc, z_scale).to_event(1))
-            mean = self.decoder.forward(z)
-            x = pyro.sample('x', dist.Bernoulli(mean).to_event(2), obs=x)
+            z = pyro.sample('z', Normal(z_loc, z_scale).to_event(1))
+            x = pyro.sample('x', decoder.predict(z), obs=x)
+        return x, z
 
-    def guide(self, x):
-        pyro.module('encoder', self.encoder)
+    def guide(self, x: torch.Tensor):
+        encoder = pyro.module('encoder', self.encoder)
         with pyro.plate('observations', x.shape[0]):
-            z_mean, z_logvar = self.encoder.forward(x)
-            z_scale = (.5 * z_logvar).exp()
-            z = pyro.sample('z', dist.Normal(z_mean, z_scale).to_event(1))
+            z = pyro.sample('z', encoder.predict(x))
+        return z
 
 
 if __name__ == '__main__':
+    from pyro.infer import SVI, Trace_ELBO
+    from pyro.optim import Adam
+    from torch.utils.data import DataLoader
+    from tqdm import tqdm
+
+    from arch.mnist import Decoder, Encoder
+    from distributions.deep import DeepBernoulli, DeepIndepNormal
+    from scripts import data_util
+
     use_cuda = True
     device = 'cuda' if use_cuda else 'cpu'
-    vae = VAE(latent_dim=10).to(device)
+
+    latent_dim = 10
+    hidden_dim = 400
+    vae = VAE(
+        decoder=DeepBernoulli(Decoder(latent_dim)),
+        encoder=DeepIndepNormal(Encoder(hidden_dim), hidden_dim, latent_dim),
+        hidden_dim=hidden_dim, latent_dim=latent_dim
+    ).to(device)
     svi = SVI(vae.model, vae.guide, Adam({'lr': 1e-3}), Trace_ELBO())
 
     data_dir = "/vol/biomedic/users/dc315/mnist/original"
@@ -51,10 +63,17 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_set, batch_size=train_batch_size, shuffle=True, **dl_kwargs)
     test_loader = DataLoader(test_set, batch_size=test_batch_size, shuffle=True, **dl_kwargs)
 
+    def get_traces(model, guide, *args, **kwargs):
+        guide_trace = pyro.poutine.trace(guide).get_trace(*args, **kwargs)
+        model_replay = pyro.poutine.replay(model, trace=guide_trace)
+        model_trace = pyro.poutine.trace(model_replay).get_trace(*args, **kwargs)
+        return model_trace
+
     n_epochs = 10
     for epoch in range(n_epochs):
         epoch_loss = 0.
         for x, _ in tqdm(train_loader, leave=False):
             x = x.to(device).unsqueeze(1) / 255.
             epoch_loss += svi.step(x)
+            print(get_traces(vae.model, vae.guide, x).format_shapes())
         print(f"Epoch {epoch}: {epoch_loss / len(train_loader.dataset)}")
