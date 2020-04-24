2576c102c3a2174c13716fa51929b9f67a9b0ac6
Nick Pawlowski <pawlowski.nick@gmail.com>
2020-04-24 18:51:51 +0100

    cnn for affinecoupling

--- arch/mnist.py
+++ arch/mnist.py
@@ -1,4 +1,7 @@
+import torch
 from torch import nn
+
+import numpy as np
 
 
 class Encoder(nn.Module):
@@ -73,6 +76,63 @@ class Decoder(nn.Module):
         x = self.fc2(x).view(-1, self.h2_nchan, 7, 7)
         x = self.conv1(x)
         return self.conv2(x)
+
+
+class BasicFlowConvNet(nn.Module):
+    def __init__(self, in_channels: int, hidden_channels: int, param_dims, context_dims: int = None):
+        super().__init__()
+        self.in_channels = in_channels
+        self.hidden_channels = hidden_channels
+
+        self.param_dims = param_dims
+        self.count_params = len(param_dims)
+        self.output_dims = sum(param_dims)
+
+        self.context_dims = context_dims
+
+        self.seq1 = nn.Sequential(
+            nn.Conv2d(in_channels, hidden_channels, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
+        self.hid = nn.Conv2d(hidden_channels + context_dims if context_dims is not None else hidden_channels, hidden_channels, kernel_size=1)
+        self.seq2 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(hidden_channels, self.output_dims, kernel_size=3, padding=1)
+        )
+
+        ends = torch.cumsum(torch.tensor(param_dims), dim=0)
+        starts = torch.cat((torch.zeros(1).type_as(ends), ends[:-1]))
+        self.param_slices = [slice(s.item(), e.item()) for s, e in zip(starts, ends)]
+
+    def forward(self, inputs, context=None):
+        # pyro affine coupling splits on the last dimenion and not the channel dimension
+        # -> we want to permute the dimensions to move the last dimension into the channel dimension
+        # and then transpose back
+
+        if not ((self.context_dims is None) == (context is None)):
+            raise ValueError(f'Given context does not match context dims: context: {context} and context_dims:{self.context_dims}')
+
+        *batch_dims, h, w, c = inputs.size()
+        num_batch = len(batch_dims)
+
+        permutation = np.array((2, 0, 1)) + num_batch
+        outputs = inputs.permute(*np.arange(num_batch), *permutation).contiguous()
+
+        outputs = self.seq1(outputs)
+        if context is not None:
+            # assuming scalar inputs [B, C]
+            context = context.view(*context.shape, 1, 1).expand(-1, -1, *outputs.shape[2:])
+            outputs = torch.cat([outputs, context], 1)
+        outputs = self.hid(outputs)
+        outputs = self.seq2(outputs)
+
+        permutation = np.array((1, 2, 0)) + num_batch
+        outputs = outputs.permute(*np.arange(num_batch), *permutation).contiguous()
+
+        if self.count_params > 1:
+            outputs = tuple([outputs[..., s] for s in self.param_slices])
+
+        return outputs
 
 
 def weights_init(m):
