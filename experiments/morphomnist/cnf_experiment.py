1b23e264fe1946adc9a1b154eb59343226929cbc
Nick Pawlowski <pawlowski.nick@gmail.com>
2020-04-24 18:53:17 +0100

    initial cnn based conditional nf model

--- experiments/morphomnist/cnf_experiment.py
+++ experiments/morphomnist/cnf_experiment.py
@@ -16,18 +16,23 @@ from pyro.distributions import Gamma, Normal, TransformedDistribution
 from pyro.distributions.torch_transform import TransformModule, ComposeTransformModule
 from pyro.distributions.conditional import ConditionalTransformedDistribution
 from pyro.distributions.transforms import (
-    Spline, ExpTransform, ComposeTransform, conditional_affine_coupling,
-    Permute, SigmoidTransform, AffineTransform
+    Spline, ExpTransform, ComposeTransform, ConditionalAffineCoupling,
+    Permute, SigmoidTransform, AffineTransform, GeneralizedChannelPermute
 )
 import torchvision
 from experiments import PyroExperiment
+from distributions.transforms.reshape import ReshapeTransform, SqueezeTransform, TransposeTransform
+from arch.mnist import BasicFlowConvNet
 
 
 class FlowModel(PyroModule):
-    def __init__(self, hidden_dim: int, num_hidden_layers: int):
+    def __init__(self, num_scales: int = 4, flows_per_scale: int = 2, preprocessing: str = 'realnvp', hidden_mult: int = 2):
         super().__init__()
-        self.hidden_dim = hidden_dim
-        self.num_hidden_layers = num_hidden_layers
+        self.num_scales = num_scales
+        self.flows_per_scale = flows_per_scale
+        self.preprocessing = preprocessing
+        self.hidden_mult = hidden_mult
+
         # TODO: This could be handled by passing a product distribution?
 
         # priors
@@ -37,8 +42,10 @@ class FlowModel(PyroModule):
         self.register_buffer('e_s_loc', torch.zeros([1, ], requires_grad=False))
         self.register_buffer('e_s_scale', torch.ones([1, ], requires_grad=False))
 
-        self.register_buffer('e_x_loc', torch.zeros([784, ], requires_grad=False))
-        self.register_buffer('e_x_scale', torch.ones([784, ], requires_grad=False))
+        self.register_buffer('e_x_loc', torch.zeros([1, 32, 32], requires_grad=False))
+        self.register_buffer('e_x_scale', torch.ones([1, 32, 32], requires_grad=False))
+
+        self.dims = {'x': 32 * 32, 'thickness': 1, 'slant': 1}
 
         # decoder parts
         # TODO:
@@ -52,18 +59,60 @@ class FlowModel(PyroModule):
         # build flow as s_affine_w * t * e_s + b -> depends on t though
 
         # realnvp or so for x
-        # TODO: replace with conv net or so
-        self.x_flow1 = conditional_affine_coupling(784, 2, [hidden_dim] * num_hidden_layers)
-        self.x_flow2 = conditional_affine_coupling(784, 2, [hidden_dim] * num_hidden_layers)
-        self.x_flow3 = conditional_affine_coupling(784, 2, [hidden_dim] * num_hidden_layers)
+        self._build_image_flow()
+
+    def _build_image_flow(self):
+        alpha = 0.05
+        num_bits = 8
+
+        self.trans_modules = ComposeTransformModule([])
+
         self.x_transforms = [
-            self.x_flow1,
-            Permute(torch.randperm(784, dtype=torch.long)),
-            self.x_flow2,
-            Permute(torch.randperm(784, dtype=torch.long)),
-            self.x_flow3,
-            SigmoidTransform()
+            ReshapeTransform((1, 32, 32), (4 ** self.num_scales, 32 // 2 ** self.num_scales, 32 // 2 ** self.num_scales))
         ]
+
+        c = 4 ** self.num_scales
+        for scale in range(self.num_scales):
+            for _ in range(self.flows_per_scale):
+                gcp = GeneralizedChannelPermute(channels=c)
+                self.trans_modules.append(gcp)
+                self.x_transforms.append(gcp)
+
+                self.x_transforms.append(TransposeTransform(torch.tensor((1, 2, 0))))
+
+                ac = ConditionalAffineCoupling(c // 2, BasicFlowConvNet(c // 2, c * self.hidden_mult, (c // 2, c // 2), 2))
+                self.trans_modules.append(ac)
+                self.x_transforms.append(ac)
+
+                self.x_transforms.append(TransposeTransform(torch.tensor((2, 0, 1))))
+
+            gcp = GeneralizedChannelPermute(channels=c)
+            self.trans_modules.append(gcp)
+            self.x_transforms.append(gcp)
+            self.x_transforms.append(SqueezeTransform().inv)
+
+            c = c // 4
+
+        gcp = GeneralizedChannelPermute(channels=1)
+        self.trans_modules.append(gcp)
+        self.x_transforms.append(gcp)
+
+        if self.preprocessing == 'glow':
+            # Map to [-0.5,0.5]
+            a1 = AffineTransform(-0.5, (1. / 2 ** num_bits))
+            preprocess_transform = [a1.inv]
+        elif self.preprocessing == 'realnvp':
+            s = SigmoidTransform()
+
+            # Map into unconstrained space as done in RealNVP
+            a1 = AffineTransform(alpha, (1 - alpha))
+
+            # Map to [0,1]
+            a2 = AffineTransform(0., (1. / 2 ** num_bits))
+
+            preprocess_transform = [s, a1.inv, a2.inv]
+
+        self.x_transforms += preprocess_transform
 
     @pyro_method
     def model(self):
@@ -80,7 +129,7 @@ class FlowModel(PyroModule):
 
         context = torch.cat([thickness, slant], 1)
 
-        x_bd = Normal(self.e_x_loc, self.e_x_scale).to_event(1)
+        x_bd = Normal(self.e_x_loc, self.e_x_scale).to_event(3)
         x_dist = ConditionalTransformedDistribution(x_bd, self.x_transforms)
         cond_x_dist = x_dist.condition(context)
 
@@ -94,19 +143,22 @@ class FlowModel(PyroModule):
         e_t = pyro.sample('e_t', t_bd)
 
         thickness = self.t_flow_transforms(e_t)
+        thickness = pyro.deterministic('thickness', thickness)
 
         s_bd = Normal(self.e_s_loc, self.e_s_scale)
         e_s = pyro.sample('e_s', s_bd)
 
         slant = AffineTransform(self.s_affine_w * thickness, self.s_affine_b)(e_s)
+        slant = pyro.deterministic('slant', slant)
 
         context = torch.cat([thickness, slant], 1)
 
-        x_bd = Normal(self.e_x_loc, self.e_x_scale).to_event(1)
+        x_bd = Normal(self.e_x_loc, self.e_x_scale).to_event(3)
         e_x = pyro.sample('e_x', x_bd)
         cond_x_transforms = ComposeTransform(ConditionalTransformedDistribution(x_bd, self.x_transforms).condition(context).transforms)
 
         x = cond_x_transforms(e_x)
+        x = pyro.deterministic('x', x)
 
         return x, thickness, slant
 
@@ -150,7 +202,9 @@ class CNFExperiment(BaseCovariateExperiment):
     def __init__(self, hparams):
         super().__init__(hparams)
 
-        self.pyro_model = FlowModel(hidden_dim=hparams.hidden_dim, num_hidden_layers=hparams.num_hidden_layers)
+        self.pyro_model = FlowModel(
+            num_scales=hparams.num_scales, flows_per_scale=hparams.flows_per_scale,
+            preprocessing=hparams.preprocessing, hidden_mult=hparams.hidden_mult)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
@@ -161,25 +215,28 @@ class CNFExperiment(BaseCovariateExperiment):
     def backward(self, *args, **kwargs):
         return super(PyroExperiment, self).backward(*args, **kwargs)
 
-    def get_logprob(self, x, thickness, slant):
+    def get_logprobs(self, x, thickness, slant):
         data = {'x': x, 'thickness': thickness, 'slant': slant}
         cond_model = pyro.condition(self.pyro_model.sample, data=data)
         model_trace = pyro.poutine.trace(cond_model).get_trace(x.shape[0])
         model_trace.compute_log_prob()
 
-        log_prob = torch.zeros([], device=x.device)
+        log_probs = {}
         for name, site in model_trace.nodes.items():
             if site["type"] == "sample" and site["is_observed"]:
-                log_prob += site["log_prob_sum"]
+                log_probs[name] = site["log_prob"].mean()
 
-        return log_prob
+        return log_probs
 
     def prep_batch(self, batch):
-        x = batch['image']
-        thickness = batch['thickness'].unsqueeze(1)
-        slant = batch['slant'].unsqueeze(1)
+        x = batch['image'].float()
+        thickness = batch['thickness'].unsqueeze(1).float()
+        slant = batch['slant'].unsqueeze(1).float()
 
-        x = x.float().reshape(-1, 784)
+        x = torch.nn.functional.pad(x, (2, 2, 2, 2))
+        x += torch.rand_like(x)
+
+        x = x.reshape(-1, 1, 32, 32)
 
         return x, thickness, slant
 
@@ -189,29 +246,45 @@ class CNFExperiment(BaseCovariateExperiment):
         if self.hparams.validate:
             self.print_trace_updates(batch)
 
-        loss = -self.get_logprob(x, thickness, slant)
+        log_probs = self.get_logprobs(x, thickness, slant)
+        loss = -torch.stack(tuple(log_probs.values())).sum()
 
-        tensorboard_logs = {'train/loss': loss}
+        lls = {('train/' + k + '_ll'): v for k, v in log_probs.items()}
+        bits_per_dim = {('train/' + k + '_bits_per_dim'): -v / self.pyro_model.dims[k] for k, v in log_probs.items()}
 
-        return {'loss': loss, 'log': tensorboard_logs}
+        tensorboard_logs = {'train/loss': loss, **bits_per_dim, **lls}
+
+        return {'loss': loss, 'log': tensorboard_logs, **lls}
 
     def validation_step(self, batch, batch_idx):
         x, thickness, slant = self.prep_batch(batch)
 
-        loss = -self.get_logprob(x, thickness, slant)
+        log_probs = self.get_logprobs(x, thickness, slant)
+        loss = -torch.stack(tuple(log_probs.values())).sum()
 
-        return {'loss': loss}
+        lls = {(k + '_ll'): v for k, v in log_probs.items()}
+        bits_per_dim = {(k + '_bits_per_dim'): -v / self.pyro_model.dims[k] for k, v in log_probs.items()}
+
+        return {'loss': loss, **lls, **bits_per_dim}
 
     def test_step(self, batch, batch_idx):
         x, thickness, slant = self.prep_batch(batch)
 
-        loss = -self.get_logprob(x, thickness, slant)
-        return {'loss': loss}
+        log_probs = self.get_logprobs(x, thickness, slant)
+        loss = -torch.stack(tuple(log_probs.values())).sum()
+
+        lls = {(k + '_ll'): v for k, v in log_probs.items()}
+        bits_per_dim = {(k + '_bits_per_dim'): -v / self.pyro_model.dims[k] for k, v in log_probs.items()}
+
+        return {'loss': loss, **lls, **bits_per_dim}
 
     def sample_images(self):
+        if self.current_epoch < 10:
+            return
+
         with torch.no_grad():
             samples, thickness, slant = self.pyro_model.sample(128)
-            samples = samples.reshape(-1, 1, 28, 28)
+            samples = samples.reshape(-1, 1, 32, 32)
             grid = torchvision.utils.make_grid(samples.data[:8], normalize=True)
             self.logger.experiment.add_image('samples', grid, self.current_epoch)
 
@@ -225,7 +298,7 @@ class CNFExperiment(BaseCovariateExperiment):
             slants = slants.repeat_interleave(3).unsqueeze(1)
 
             samples, *_ = pyro.condition(self.pyro_model.sample, data={'thickness': thicknesses, 'slant': slants})(9)
-            samples = samples.reshape(-1, 1, 28, 28)
+            samples = samples.reshape(-1, 1, 32, 32)
             grid = torchvision.utils.make_grid(samples.data, normalize=True, nrow=3)
             self.logger.experiment.add_image('cond_samples', grid, self.current_epoch)
 
@@ -233,7 +306,7 @@ class CNFExperiment(BaseCovariateExperiment):
             x = x[:8]
             thickness = thickness[:8]
             slant = slant[:8]
-            x_ = x.reshape(-1, 1, 28, 28)
+            x_ = x.reshape(-1, 1, 32, 32)
 
             grid = torchvision.utils.make_grid(x_, normalize=True)
             self.logger.experiment.add_image('input', grid, self.current_epoch)
@@ -242,21 +315,20 @@ class CNFExperiment(BaseCovariateExperiment):
             slant = slant.to(samples.device)
 
             e_t, e_s, e_x = self.pyro_model.infer(thickness, slant, x)
+            data = {'e_t': e_t, 'e_s': e_s, 'e_x': e_x}
 
-            counter_e_t = self.pyro_model.infer_e_t(thickness + 2)
-            counter, *_ = pyro.condition(self.pyro_model.sample_scm, data={'e_t': counter_e_t, 'e_s': e_s, 'e_x': e_x})(8)
-            counter = counter.reshape(-1, 1, 28, 28).cpu().data
-            grid = torchvision.utils.make_grid(torch.cat([x_, counter], 0), normalize=True)
+            counter, *_ = pyro.poutine.do(pyro.condition(self.pyro_model.sample_scm, data=data), data={'thickness': thickness + 2})(8)
+            counter = counter.reshape(-1, 1, 32, 32).cpu().data
+            grid = torchvision.utils.make_grid(torch.cat([x_.cpu(), counter], 0), normalize=True)
             self.logger.experiment.add_image('counter_thickness', grid, self.current_epoch)
 
             measured_thickness, measured_slant = self.measure_image(counter)
             self.logger.experiment.add_scalar('counter_thickness/thickness_mae', torch.mean(torch.abs(thickness.cpu() - measured_thickness + 2)), self.current_epoch)
             self.logger.experiment.add_scalar('counter_thickness/slant_mae', torch.mean(torch.abs(slant.cpu() - measured_slant)), self.current_epoch)
 
-            counter_e_s = self.pyro_model.infer_e_s(thickness, slant + 20)
-            counter, *_ = pyro.condition(self.pyro_model.sample, data={'e_t': e_t, 'e_s': counter_e_s, 'e_x': e_x})(8)
-            counter = counter.reshape(-1, 1, 28, 28).cpu().data
-            grid = torchvision.utils.make_grid(torch.cat([x_, counter], 0), normalize=True)
+            counter, *_ = pyro.poutine.do(pyro.condition(self.pyro_model.sample, data=data), data={'slant': slant + 20})(8)
+            counter = counter.reshape(-1, 1, 32, 32).cpu().data
+            grid = torchvision.utils.make_grid(torch.cat([x_.cpu(), counter], 0), normalize=True)
             self.logger.experiment.add_image('counter_slant', grid, self.current_epoch)
 
             measured_thickness, measured_slant = self.measure_image(counter)
@@ -275,8 +347,10 @@ if __name__ == '__main__':
     parser._action_groups[1].title = 'lightning_options'
 
     experiment_group = parser.add_argument_group('experiment')
-    experiment_group.add_argument('--hidden_dim', default=400, type=int, help="hidden dimension of model (defaults to 400)")
-    experiment_group.add_argument('--num_hidden_layers', default=2, type=int, help="number of hidden layers of model (defaults to 2)")
+    experiment_group.add_argument('--num_scales', default=4, type=int, help="number of scales (defaults to 4)")
+    experiment_group.add_argument('--flows_per_scale', default=2, type=int, help="number of flows per scale (defaults to 2)")
+    experiment_group.add_argument('--preprocessing', default='realnvp', type=str, help="type of preprocessing", choices=['realnvp', 'glow'])
+    experiment_group.add_argument('--hidden_mult', default=2, type=int, help="multiplier for num hidden channels in convnet (defaults to 2)")
     experiment_group.add_argument('--lr', default=1e-4, type=float, help="latent dimension of model (defaults to 1e-4)")
     experiment_group.add_argument('--validate', default=False, action='store_true', help="latent dimension of model (defaults to False)")
 
@@ -294,7 +368,6 @@ if __name__ == '__main__':
     hparams = groups['experiment']
 
     trainer = Trainer.from_argparse_args(lightning_args)
-    print(trainer.on_gpu)
 
     experiment = CNFExperiment(hparams)
 
